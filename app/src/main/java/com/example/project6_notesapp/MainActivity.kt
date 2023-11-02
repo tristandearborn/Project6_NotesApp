613abd0d0fbe155f4012c4b301d3eb49a87c4fb5
tdearbor <tdearbor@iu.edu>
2023-11-01 23:51:04 -0400

    changed note

--- app/src/main/java/com/example/project6_notesapp/MainActivity.kt
+++ app/src/main/java/com/example/project6_notesapp/MainActivity.kt
@@ -11,12 +11,12 @@ import com.example.project6_notesapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //this screen is the host of all fragments. it displays them all.
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) //initiates binding
         val view = binding.root
         setContentView(view)
-        supportFragmentManager.beginTransaction().replace(R.id.nav,homeScreen()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.nav,loginScreen()).commit()
 
     }
 
